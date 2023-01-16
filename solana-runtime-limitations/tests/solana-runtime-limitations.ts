f9ec9a9ae206bea67a548dee19c7984bc81390f2
Ondra Chaloupka <chalda@chainkeepers.io>
2023-01-16 07:42:24 +0100

    Transaction size limit test

--- solana-runtime-limitations/tests/solana-runtime-limitations.ts
+++ solana-runtime-limitations/tests/solana-runtime-limitations.ts
@@ -1,7 +1,8 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { AnchorError, Program } from "@project-serum/anchor";
 import { Keypair } from "@solana/web3.js";
 import { SolanaRuntimeLimitations } from "../target/types/solana_runtime_limitations";
+import { expect } from 'chai'
 
 describe("solana-runtime-limitations", () => {
   // Configure the client to use the local cluster.
@@ -10,9 +11,11 @@ describe("solana-runtime-limitations", () => {
   const program = anchor.workspace.SolanaRuntimeLimitations as Program<SolanaRuntimeLimitations>;
   const provider = anchor.getProvider() as anchor.AnchorProvider
 
-  it("transaction size limitation", async () => {
+  it("transaction size limitation, size limit passed", async () => {
     const accountKey = Keypair.generate()
-    const data = "a".repeat(1000)
+    // Maximal transaction size is 1232; this is maximal lenght of string to fit
+    const data = "a".repeat(917)
+    console.log("data len", data.length)
     const tx = await program.methods
       .dataSizeLimit({
         data,
@@ -23,7 +26,27 @@ describe("solana-runtime-limitations", () => {
         payer: provider.publicKey
       })
       .signers([accountKey])
-      .rpc();
-    console.log("Your transaction signature", tx);
+      .rpc()
+    console.log("Sent tx", tx)
+  });
+
+  it("transaction size limitation", async () => {
+    const accountKey = Keypair.generate()
+    const data = "a".repeat(918)
+    try {
+      const tx = await program.methods
+        .dataSizeLimit({
+          data,
+          space: 8 + 4 + data.length // anchor delimitar, space for lenght size, data
+        })
+        .accounts({
+          dataAccount: accountKey.publicKey,
+          payer: provider.publicKey
+        })
+        .signers([accountKey])
+        .rpc()
+    } catch (e) {
+      expect((e as Error).message.includes("Transaction too large"), e)
+    }
   });
 });
